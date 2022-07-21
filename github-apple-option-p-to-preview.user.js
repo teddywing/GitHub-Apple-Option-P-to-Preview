60dd527a0454604b6b3acd274e4f66d5b8f116b1
Teddy Wing <git@teddywing.com>
2022-07-21 20:53:29 +0200

    Add copyright notice
    I guess I didn't bother with this when I first wrote the script.

--- github-apple-option-p-to-preview.user.js
+++ github-apple-option-p-to-preview.user.js
@@ -7,6 +7,8 @@
 // @grant       none
 // ==/UserScript==
 
+// Copyright (c) 2017, 2022  Teddy Wing
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
